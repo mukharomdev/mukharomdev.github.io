3c31dc47f8572150b431ef6fad137eab10282479
pelatge <pelat.g29@gmail.com>
2025-02-18 08:14:31 +0700

    edit workflows

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
+  output: 'export'
+
   reactStrictMode: true,
   images: {
     unoptimized: true, // Disable default image optimization
   },
   assetPrefix: isProd ? '/mukharomdev.github.io/' : '',
   basePath: isProd ? '/mukharomdev.github.io' : '',
-  output: 'export'
-};
+  };
 
 export default nextConfig;
