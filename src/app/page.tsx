93c203304a28422fa355607b81cecd9e40907469
mukharomdev <mukharomdev@gmail.com>
2025-02-21 16:10:36 +0700

    add folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
-import { Button } from "flowbite-react";
+import Container from "@/app/_components/container";
+import { HeroPost } from "@/app/_components/hero-post";
+import { Intro } from "@/app/_components/intro";
+import { MoreStories } from "@/app/_components/more-stories";
+import { getAllPosts } from "@/lib/api";
 
 export default function Index() {
+  const allPosts = getAllPosts();
+
+  const heroPost = allPosts[0];
+
+  const morePosts = allPosts.slice(1);
+
   return (
-    <div>
-     <h1>root app </h1>
-    </div>
+    <main>
+      <Container>
+        <Intro />
+        <HeroPost
+          title={heroPost.title}
+          coverImage={heroPost.coverImage}
+          date={heroPost.date}
+          author={heroPost.author}
+          slug={heroPost.slug}
+          excerpt={heroPost.excerpt}
+        />
+        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+      </Container>
+    </main>
   );
 }
