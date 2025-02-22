967f3a28e7f7d5154186843a8fc49f07f2be3eac
mukharomdev <mukharomdev@gmail.com>
2025-02-23 05:34:28 +0700

    add package

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
+// const path = require('path');
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  output: 'export',
+
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/mukharomdev.github.io/' : '',
+  basePath: isProd ? '/mukharomdev.github.io' : '',
+  
+  };
 
 export default nextConfig;
+
+// webpack: (config) => {
+//     config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+//     return config;
+//   },
