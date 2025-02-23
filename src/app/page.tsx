a7f870537c0e60d2a5c5985d7855d4b53723a74b
Mukharomdev <123336003+mukharomdev@users.noreply.github.com>
2025-02-24 02:59:43 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@
 export default function Page() {
   return (
     <div className="min-h-screen animate-gradient-xy bg-gradient-to-br from-gray-50 via-gray-100 to-gray-200 dark:from-gray-900 dark:via-gray-800 dark:to-gray-900">   
-    <h1>maaf sedang dalam [engembangam</h1>
+    <h1 className="text-lg">maaf sedang dalam pengembangam</h1>
     </div>
   )
 }
