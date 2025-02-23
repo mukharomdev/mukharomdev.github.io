d5526bf8525ae6781fba3b18f2aa05472c975777
mukharomdev <mukharomdev@gmail.com>
2025-02-23 08:04:44 +0700

    add package

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  assetPrefix : "/mukharomdev.github.io/",
+  basePrefix : "mukharomdev.github.io",
+};
+
+export default nextConfig;
