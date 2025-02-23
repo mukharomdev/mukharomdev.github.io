6ffaec1c3cb6d7d7dda2e4583699834062638b6b
mukharomdev <mukharomdev@gmail.com>
2025-02-24 03:54:27 +0700

    edit next.config

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output : "export",
-  assetPrefix : "/mukharomdev.github.io/",
-  basePath : "/mukharomdev.github.io",
+  
 };
 
 export default nextConfig;
