2936bda026821c5c2337a58153be423c5e5a9b9e
Mukharomdev <123336003+mukharomdev@users.noreply.github.com>
2025-02-23 11:15:01 +0000

    edit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
