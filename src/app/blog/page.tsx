44786aeae6acee148db1121b67ed2cc5770e7474
mukharomdev <mukharomdev@gmail.com>
2025-02-26 01:25:27 +0700

    update src

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,24 +1,55 @@
-import Link from 'next/link';
+// import Link from 'next/link';
 
-const posts = [
-  { id: 1, title: 'Post 1', slug: 'post-1' },
-  { id: 2, title: 'Post 2', slug: 'post-2' },
-  { id: 3, title: 'Post 3', slug: 'post-3' },
-];
+// const posts = [
+//   { id: 1, title: 'Post 1', slug: 'post-1' },
+//   { id: 2, title: 'Post 2', slug: 'post-2' },
+//   { id: 3, title: 'Post 3', slug: 'post-3' },
+// ];
 
-export default async function Blog() {
+// export default async function Blog() {
+//   return (
+//     <div>
+//       <h1>Blog Posts</h1>
+//       <ul>
+//         {posts.map((post) => (
+//           <li key={post.id}>
+//             <Link href={`/blog/${post.slug}`}>
+//               {post.title}
+//             </Link>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+import Container from "@/app/_components/container";
+import { HeroPost } from "@/app/_components/hero-post";
+import { Intro } from "@/app/_components/intro";
+import { MoreStories } from "@/app/_components/more-stories";
+import { getAllPosts } from "@/lib/api";
+
+export default function Index() {
+  const allPosts = getAllPosts();
+
+  const heroPost = allPosts[0];
+
+  const morePosts = allPosts.slice(1);
+
   return (
-    <div>
-      <h1>Blog Posts</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <Link href={`/blog/${post.slug}`}>
-              {post.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <main>
+      <Container>
+        <Intro />
+        <HeroPost
+          title={heroPost.title}
+          coverImage={heroPost.coverImage}
+          date={heroPost.date}
+          author={heroPost.author}
+          slug={heroPost.slug}
+          excerpt={heroPost.excerpt}
+        />
+        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+      </Container>
+    </main>
   );
 }
