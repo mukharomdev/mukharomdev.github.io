dd402ed61704dd04286f9839961ae1c627bb97c5
Mukharomdev <123336003+mukharomdev@users.noreply.github.com>
2025-02-25 10:27:53 +0000

    "update src/app/blog"

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,12 +1,19 @@
 import { notFound } from 'next/navigation';
 
+
 const posts = [
   { id: 1, title: 'Post 1', slug: 'post-1', content: 'Ini adalah konten dari Post 1' },
   { id: 2, title: 'Post 2', slug: 'post-2', content: 'Ini adalah konten dari Post 2' },
   { id: 3, title: 'Post 3', slug: 'post-3', content: 'Ini adalah konten dari Post 3' },
 ];
 
-export default  function BlogPost({ params }: { params: { slug: string } }) {
+// Tipe untuk params
+type PageProps = {
+  params: { slug: string };
+};
+
+
+export default  function BlogPost({ params }: PageProps) {
   const { slug } = params;
    
   const post = posts.find((p) => p.slug === slug);
