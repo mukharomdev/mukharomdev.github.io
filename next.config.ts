acd1eb970263bc69ceacf232e1b13c025c480e67
Mukharomdev <123336003+mukharomdev@users.noreply.github.com>
2025-02-25 21:07:53 +0000

    "update next.config"

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,11 @@ const nextConfig: NextConfig = {
 
   output:"export",
   images: { unoptimized: true },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 
 };
 
