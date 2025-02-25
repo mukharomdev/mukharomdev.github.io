3600414dd98e125182b09f37194da7f64f52b91e
Mukharomdev <123336003+mukharomdev@users.noreply.github.com>
2025-02-25 10:21:35 +0000

    "update src/app/blog"

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -6,9 +6,9 @@ const posts = [
   { id: 3, title: 'Post 3', slug: 'post-3', content: 'Ini adalah konten dari Post 3' },
 ];
 
-export default async function BlogPost({ params }: { params: { slug: string } }) {
-  const { slug } = await params;
-
+export default  function BlogPost({ params }: { params: { slug: string } }) {
+  const { slug } = params;
+   
   const post = posts.find((p) => p.slug === slug);
 
   if (!post) {
