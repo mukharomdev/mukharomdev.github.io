051b345d378a2bbc4ad68e11e8ff8782504732e0
Mukharomdev <123336003+mukharomdev@users.noreply.github.com>
2025-02-26 22:44:43 +0000

    "update lib/markdowntohtml"

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,7 +1,18 @@
-import { remark } from "remark";
-import html from "remark-html";
+//import { remark } from "remark";
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkGfm from "remark-gfm"; // Untuk dukungan tabel
+import remarkHtml from "remark-html";
+
+
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown);
+  //const result = await remark().use(html).process(markdown);
+  const result = await unified()
+    .use(remarkParse) // Parse Markdown
+    .use(remarkGfm) // Tambahkan dukungan tabel GFM
+    .use(remarkHtml) // Konversi ke HTML
+    .process(markdown);
+
   return result.toString();
 }
