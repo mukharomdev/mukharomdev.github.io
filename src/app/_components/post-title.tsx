3a23bb47ae9c069a0bfe078b178d21297e04ff05
Mukharomdev <123336003+mukharomdev@users.noreply.github.com>
2025-03-01 06:31:28 +0700

    Update post-title.tsx

--- src/app/_components/post-title.tsx
+++ src/app/_components/post-title.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export function PostTitle({ children }: Props) {
   return (
-    <h1 className="text-2xl text-center md:text-2xl lg:text-4xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">
+    <h1 className="text-2xl text-center md:text-3xl lg:text-4xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">
       {children}
     </h1>
   );
