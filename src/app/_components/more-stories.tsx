d1f2c192f85601bac1203d352f0775fa78620664
Mukharomdev <123336003+mukharomdev@users.noreply.github.com>
2025-03-01 00:56:04 +0700

    Update more-stories.tsx

--- src/app/_components/more-stories.tsx
+++ src/app/_components/more-stories.tsx
@@ -16,11 +16,7 @@ export function MoreStories({ posts }: Props) {
           <PostPreview
             key={post.slug}
             title={post.title}
-            coverImage={post.coverImage}
-            date={post.date}
-            author={post.author}
-            slug={post.slug}
-            excerpt={post.excerpt}
+            
           />
         ))}
       </div>
